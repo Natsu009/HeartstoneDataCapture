34499332348cc3492a6f330ddaba8953d9cebd28
Natsu009 <paramktrivedi1@gmail.com>
2018-11-06 19:39:21 -0500

    New module data duplication

---
+++ HeartstoneDataCapture/Controllers/winRateController.cs
@@ -0,0 +1,347 @@
+﻿using CsvHelper;
+using HeartstoneDataCapture.Models;
+using HeartstoneDataCapture.Providers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HeartstoneDataCapture.Controllers
+{
+    class winRateController
+    {
+        public cardInfoProvider allCards = new cardInfoProvider();
+
+        /// <summary>
+        /// This method will scrape the value from text files
+        /// The values of Friendly Decks will be stored
+        /// </summary>
+        public List<List<List<int>>> scrapeFriendFromText(int deckPerGen, int genFolderDups , int genFolderCount)
+        {
+            List<List<List<int>>> friendDecksFinal = new List<List<List<int>>>();
+            Dictionary<string, cardDefs> cards = allCards.getAllCards();
+
+            Console.WriteLine("*************** Get the friend decks from Gen Folders ******************\n\n");
+            for(int total = 0; total < genFolderCount; total++)
+            {
+                Console.WriteLine("Scraping friend decks info for gen Folder no: " + total);
+                for (int i = 0; i < genFolderDups; i++)
+                {
+                    if (i != 0) continue;
+                    List<List<int>> friendDecks = new List<List<int>>();
+                    for (int j = 0; j < deckPerGen; j++)
+                    {
+                        List<int> friendDeckTemp = new List<int>();
+                        string fileName = "";
+
+                        if (total == 0)
+                        {
+                            fileName = "C:/Development/HeartstoneDataCapture/HeartstoneDataCapture/Data/FriendDeckDuplicate/gen-" + i + "/Overall/Deck" + j + "/" + 0 + "-" + j + ".txt";
+                        }
+                        else
+                        {
+                            fileName = "C:/Development/HeartstoneDataCapture/HeartstoneDataCapture/Data/FriendDecksData/gen" + (total + 1) + "-"+i+"/Overall/Deck" + j + "/" + 0 + "-" + j + ".txt";
+                        }
+
+                        
+                        if (j % 100 == 0 && j < deckPerGen)
+                        {
+
+                            string lines = File.ReadAllText(fileName);
+                            string[] items = lines.Split('*');
+
+                            int count = 0;
+                            List<friendDeckModel> fModelWhole = new List<friendDeckModel>();
+
+                            foreach (string item in items)
+                            {
+
+                                friendDeckModel fModel = new friendDeckModel();
+                                if (count < 30)
+                                {
+                                    fModel.cardName = cards.GetValueOrDefault(item).dbfId;
+                                    fModel.cost = cards.GetValueOrDefault(item).cost;
+                                    fModelWhole.Add(fModel);
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                                count++;
+                            }
+
+                            fModelWhole = fModelWhole.OrderBy(x => x.cost).ToList();
+
+                            foreach (friendDeckModel fMod in fModelWhole)
+                            {
+                                friendDeckTemp.Add(fMod.cardName);
+                            }
+                            friendDecks.Add(friendDeckTemp);
+
+                        }
+
+                    }
+                    friendDecksFinal.Add(friendDecks);
+
+                }
+            }
+            
+            return friendDecksFinal;
+        }
+
+
+        public List<List<int>> scrapeEnemyFromText(int genFolderDups, int genFolderCount)
+        {
+            Console.WriteLine("*************** Get the enemy decks from gen folders ******************\n\n");
+            List<List<int>> enemyDecks = new List<List<int>>();
+            Dictionary<string, cardDefs> cardsList = allCards.getAllCards();
+
+            for (int i = 0; i < genFolderCount; i++)
+            {
+                Console.WriteLine("Scraping enemy decks info for gen Folder no: " + i);
+                List<int> enemyDeckTemp = new List<int>();
+                for (int j = 0; j < genFolderDups; j++) {
+                    string fileName = "";
+
+                    if (i == 0)
+                    {
+                        fileName = "C:/Development/HeartstoneDataCapture/HeartstoneDataCapture/Data/FriendDeckDuplicate/gen-" + i + "/EnemyDeck.txt";
+                    }
+                    else
+                    {
+                        fileName = "C:/Development/HeartstoneDataCapture/HeartstoneDataCapture/Data/FriendDeckDuplicate/gen" + (i + 1) + "-0/EnemyDeck.txt";
+                    }
+
+                    if (j != 0) continue;
+
+                    string lines = File.ReadAllText(fileName);
+
+                    string[] content = lines.Split('\n');
+
+                    string[] cards = content[1].Split('*');
+
+                    List<friendDeckModel> fModelWhole = new List<friendDeckModel>();
+                    int count = 0;
+                    foreach (string card in cards)
+                    {
+                        friendDeckModel fModel = new friendDeckModel();
+                        if (count < 30)
+                        {
+
+                            fModel.cardName = cardsList.GetValueOrDefault(card).dbfId;
+                            fModel.cost = cardsList.GetValueOrDefault(card).cost;
+                            fModelWhole.Add(fModel);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        count++;
+                    }
+
+                    fModelWhole = fModelWhole.OrderBy(x => x.cost).ToList();
+
+                    foreach (friendDeckModel fMod in fModelWhole)
+                    {
+                        enemyDeckTemp.Add(fMod.cardName);
+                    }
+                }
+                enemyDecks.Add(enemyDeckTemp);
+
+
+            }
+
+            return enemyDecks;
+
+        }
+
+        /// <summary>
+        /// This method will calculate the WinRate of each and every decks on the basis of the total GamePlayed
+        /// </summary>
+        /// <param name="deckPerGen"></param>
+        /// <param name="genFolderDups"></param>
+        /// <param name="genFolderCount"></param>
+        /// <param name="gamePerDeck"></param>
+        /// <returns></returns>
+        public List<double> getWinRate(int deckPerGen, int genFolderDups, int genFolderCount , int gamePerDeck) {
+
+            Console.WriteLine("*************** Calculate the WinRate of the Folders ******************\n\n");
+
+            List<double> finalWinRate = new List<double>();
+            for(int i = 0; i < genFolderCount; i++)
+            {
+                List<List<int>> dupsWinRate = new List<List<int>>();
+                for (int j = 0; j < genFolderDups; j++)
+                {
+                    List<int> tempdupsWinRate = new List<int>();
+                    int totalWinRate = 0; int deckIncrementCount = 0;
+                    for (int k = 0; k < deckPerGen; k++)
+                    {
+                        
+                        string fileName = "";
+
+                        if (i == 0)
+                        {
+                            fileName = "C:/Development/HeartstoneDataCapture/HeartstoneDataCapture/Data/FriendDeckDuplicate/gen-" + j + "/Overall/Deck" + k + "/" + 0 + "-" + k + ".txt";
+                        }
+                        else
+                        {
+                            fileName = "C:/Development/HeartstoneDataCapture/HeartstoneDataCapture/Data/FriendDeckDuplicate/gen" + (i + 1) + "-"+j+"/Overall/Deck" + k + "/" + 0 + "-" + k + ".txt";
+                        }
+
+                        
+
+                        string lines = File.ReadAllText(fileName);
+                        string[] items = lines.Split('/');
+                        
+                        // Get the result
+                        if (items[0].Contains("Player1: WON"))
+                        {
+                             totalWinRate++;
+                            deckIncrementCount++;
+                        }
+                        else
+                        {
+                            deckIncrementCount++;
+                        }
+
+                        // Reset the counter
+                        if (deckIncrementCount == 100)
+                        {
+                            tempdupsWinRate.Add(totalWinRate);
+                            totalWinRate = 0;
+                            deckIncrementCount = 0;
+                        }
+
+                    }
+
+                    dupsWinRate.Add(tempdupsWinRate);
+                }
+
+                // Calculate the percentage
+                for(int mainStruc = 0; mainStruc < 100 ; mainStruc++)
+                {
+                    int winRate = 0;
+                    for(int subStruc = 0; subStruc < genFolderDups; subStruc++)
+                    {
+                        winRate += dupsWinRate[subStruc][mainStruc];
+                    }
+                    finalWinRate.Add((winRate/gamePerDeck)*100 );
+                }
+            }
+            return finalWinRate;
+        }
+
+        /// <summary>
+        /// This method will convert the Friendly Decks and their corresponding enemydeck into tuple
+        /// </summary>
+        /// <param name="friendDecksFinal"></param>
+        /// <param name="enemyDecks"></param>
+        /// <returns></returns>
+        public Dictionary<List<int>, List<int>> convertToTuple(List<List<List<int>>> friendDecksFinal, List<List<int>> enemyDecks, int genFolderCount, int iterable)
+        {
+            Console.WriteLine("*************** Combine the friend deck with their respective enmy deck ******************");
+            Dictionary<List<int>, List<int>> tuple = new Dictionary<List<int>, List<int>>();
+            int flag = 0; int count = 0;
+
+            foreach (List<List<int>> friendDeck in friendDecksFinal)
+            {
+                foreach (List<int> friendDeckTemp in friendDeck)
+                {
+                    if (count != 0 && count % iterable == 0 && count < (genFolderCount * iterable))
+                    {
+                        flag++;
+                    }
+
+                    if (flag == genFolderCount)
+                    {
+                        return tuple;
+                    }
+                    else
+                    {
+                        tuple.Add(friendDeckTemp, enemyDecks[flag]);
+                    }
+
+                    count++;
+                }
+
+            }
+
+            return tuple;
+        }
+
+
+        public string convertToFinalStruct(Dictionary<List<int>, List<int>> tuple, List<double> playerStat, int genFolderCount, int iterable)
+        {
+            Console.WriteLine("*************** Combine the friend deck with their respective enemy deck and win and Lose stat ******************\n\n");
+            string jsonStruct = "";
+
+            List<dupStructure> cStruct = new List<dupStructure>();
+            double[] pStat = playerStat.ToArray();
+
+            int count = 0;
+            foreach (var tunpleKey in tuple.Keys)
+            {
+                dupStructure cStructTemp = new dupStructure();
+
+                if (count < (genFolderCount * iterable))
+                {
+                    cStructTemp.FriendDeck = tunpleKey;
+                    cStructTemp.EnemyDeck = tuple.GetValueOrDefault(tunpleKey);
+                    cStructTemp.WinRate = pStat[count];
+                }
+
+                cStruct.Add(cStructTemp);
+                count++;
+            }
+
+            jsonStruct = JsonConvert.SerializeObject(cStruct);
+
+            /* Save the JSON string in a FinalDataJSON.json */
+            string fileName = "C:/Development/HeartstoneDataCapture/HeartstoneDataCapture/Data/FinalData/FinalDataWinRateJSON.json";
+            System.IO.File.WriteAllText(fileName, jsonStruct);
+
+            return jsonStruct;
+        }
+
+
+        public void convertToCsv(string jsonContent)
+        {
+            Console.WriteLine("*************** Conver the JSON to CSV ******************\n\n");
+            DataTable dtOne = JsonConvert.DeserializeObject<DataTable>(jsonContent);
+            string delimiter = ",";
+
+            StringWriter csvString = new StringWriter();
+            using (var csv = new CsvWriter(csvString))
+            {
+                csv.Configuration.Delimiter = delimiter;
+
+                using (var dt = dtOne)
+                {
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        csv.WriteField(column.ColumnName);
+                    }
+                    csv.NextRecord();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        for (var i = 0; i < dt.Columns.Count; i++)
+                        {
+                            csv.WriteField(row[i]);
+                        }
+                        csv.NextRecord();
+                    }
+                }
+            }
+
+            /* Save the JSON string in a FinalDataJSON.json */
+            string fileName = "C:/Development/HeartstoneDataCapture/HeartstoneDataCapture/Data/FinalData/FinalDataWinRate.csv";
+            System.IO.File.WriteAllText(fileName, csvString.ToString());
+
+        }
+    }
+}
--- HeartstoneDataCapture/Program.cs
+++ HeartstoneDataCapture/Program.cs
@@ -20,48 +20,52 @@ namespace HeartstoneDataCapture
             int genFolderCount = 10;
             int iterable = 100;
 
+            int deckPerGen = 10000;
+            int genFolderDups = 4;
+
             opponentDeckController opController = new opponentDeckController();
             texttocsvController ttc = new texttocsvController();
             texttocsvCostController ttcc = new texttocsvCostController();
             oneHotController onc = new oneHotController();
             cardInfoProvider cPro = new cardInfoProvider();
             dataManipulatorController dataManipulator = new dataManipulatorController();
+            winRateController winc = new winRateController();
 
             /**Get the data of all the cards**/
             Dictionary<int, cardDefs> cards = cPro.getAllCardsWithId();
 
             /******************************This section is used to convert JSON to Txt*********************************/
-            List<opponentModel> opModel = opController.convertJSONToDeck();
-            opController.convertJSONToText(opModel);
+            //List<opponentModel> opModel = opController.convertJSONToDeck();
+            //opController.convertJSONToText(opModel);
 
             /******************************This section is used to convert Txt to Csv*********************************/
             Console.WriteLine("***********************************************************************************");
             Console.WriteLine("                          TXT to CSV  Controller                                   ");
-            Console.WriteLine("***********************************************************************************\n");
+            Console.WriteLine("***********************************************************************************\n\n");
 
-            List<List<List<int>>> friendDeck =   ttc.scrapeFriendFromText(genFolderCount , iterable);
-            List<List<int>> enemyDeck = ttc.scrapeEnemyFromText(genFolderCount);
-            Dictionary<List<int>, List<int>> tuple = ttc.convertToTuple(friendDeck , enemyDeck , genFolderCount , iterable);
-            List<string> playerStat = ttc.getPlayerResult(genFolderCount , iterable);
+            //List<List<List<int>>> friendDeck =   ttc.scrapeFriendFromText(genFolderCount , iterable);
+            //List<List<int>> enemyDeck = ttc.scrapeEnemyFromText(genFolderCount);
+            //Dictionary<List<int>, List<int>> tuple = ttc.convertToTuple(friendDeck , enemyDeck , genFolderCount , iterable);
+            //List<string> playerStat = ttc.getPlayerResult(genFolderCount , iterable);
 
             // Convert the data into JSON Format    
-            string finalJSON = ttc.convertToFinalStruct(tuple , playerStat , genFolderCount , iterable);
+            //string finalJSON = ttc.convertToFinalStruct(tuple , playerStat , genFolderCount , iterable);
 
             // Convert the JSON string to CSV file
-            ttc.convertToCsv(finalJSON);
+            //ttc.convertToCsv(finalJSON);
 
             /******************************This section is used to convert Txt to Csv (with Cost)*********************************/
             Console.WriteLine("***********************************************************************************");
             Console.WriteLine("                          TEXT to CSV (with Cost)  Controller                      ");
-            Console.WriteLine("***********************************************************************************\n");
+            Console.WriteLine("***********************************************************************************\n\n");
 
-            List<List<List<friendDeckModel>>> friendDeckCost =   ttcc.scrapeFriendFromText(genFolderCount , iterable);
-            List<List<friendDeckModel>> enemyDeckCost = ttcc.scrapeEnemyFromText(genFolderCount,iterable);
-            Dictionary<List<friendDeckModel>, List<friendDeckModel>> tupleCost = ttcc.convertToTuple(friendDeckCost , enemyDeckCost, genFolderCount , iterable);
-            List<string> playerStatCost = ttcc.getPlayerResult(genFolderCount , iterable);
+            //List<List<List<friendDeckModel>>> friendDeckCost =   ttcc.scrapeFriendFromText(genFolderCount , iterable);
+            //List<List<friendDeckModel>> enemyDeckCost = ttcc.scrapeEnemyFromText(genFolderCount,iterable);
+            //Dictionary<List<friendDeckModel>, List<friendDeckModel>> tupleCost = ttcc.convertToTuple(friendDeckCost , enemyDeckCost, genFolderCount , iterable);
+            //List<string> playerStatCost = ttcc.getPlayerResult(genFolderCount , iterable);
 
             // Convert the data into JSON Format    
-            string finalJSONCost = ttcc.convertToFinalStruct(tupleCost , playerStatCost , genFolderCount , iterable);
+            //string finalJSONCost = ttcc.convertToFinalStruct(tupleCost , playerStatCost , genFolderCount , iterable);
 
 
             // Convert the JSON string to CSV file
@@ -70,23 +74,31 @@ namespace HeartstoneDataCapture
             /****************************This section will convert the data to OneHot Representation****************************/
             Console.WriteLine("***********************************************************************************");
             Console.WriteLine("                          One Hot Converter Controller                             ");
-            Console.WriteLine("***********************************************************************************\n");
+            Console.WriteLine("***********************************************************************************\n\n");
 
-            Dictionary<string, string> CardPool = onc.GetCardPoolData();
-            string oneHotStr = onc.convertJSONToOneHot1(finalJSON , CardPool);
-            onc.convertToCsv(oneHotStr);
+            //Dictionary<string, string> CardPool = onc.GetCardPoolData();
+            //string oneHotStr = onc.convertJSONToOneHot1(finalJSON , CardPool);
+            //onc.convertToCsv(oneHotStr);
             //convertJSONToOneHot2(finalJSONCost);
 
             /****************************This section will add duplicate the data and store them in a text file ****************************/
             Console.WriteLine("***********************************************************************************");
             Console.WriteLine("                          Duplicate Data  Controller                               ");
-            Console.WriteLine("***********************************************************************************\n");
+            Console.WriteLine("***********************************************************************************\n\n");
 
 
-            Dictionary<int, List<List<string>>> deckDataDict = dataManipulator.populateDeckData(genFolderCount , iterable);
-            dataManipulator.storeDatatoTxt(deckDataDict, genFolderCount, iterable);
-            dataManipulator.makeDuplicates(genFolderCount , 4);
+            //Dictionary<int, List<List<string>>> deckDataDict = dataManipulator.populateDeckData(genFolderCount , iterable);
+            //dataManipulator.storeDatatoTxt(deckDataDict, genFolderCount, iterable);
+            //dataManipulator.makeDuplicates(genFolderCount , 4);
 
+            /****************************This section will calcuate the winrate for each deck ****************************/
+            Console.WriteLine("***********************************************************************************");
+            Console.WriteLine("                               WinRate  Controller                                 ");
+            Console.WriteLine("***********************************************************************************\n\n");
+
+            List<List<List<int>>>  friendDeckDups = winc.scrapeFriendFromText(10000 , 4 , 1);
+            List<List<int>>  enemyDeckDups = winc.scrapeEnemyFromText(1 , 1);
+            List<double> winRate = winc.getWinRate(10000, 3, 1, 400);
         }
     }
 }
